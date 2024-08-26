18a1a1c8cabc171f8444b8492e65c8b45515bfda
Alexander <oBelyash@yandex.ru>
2024-08-26 19:41:18 +0300

    Initial Commit

---
+++ Quitlet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quitlet
+//
+//  Created by Александр Андреев on 26.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quitlet/QuitletApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitletApp.swift
+//  Quitlet
+//
+//  Created by Александр Андреев on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuitletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
